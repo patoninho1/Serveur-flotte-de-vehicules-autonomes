b6925b18a890fdd079639fbe8c85b3aa60126a50
Samy <zobiri_s@esme.fr>
2016-12-29 20:34:25 +0100

    add google maps on testing client

--- client.js
+++ client.js
@@ -1,7 +1,12 @@
+var map;
+var markersVehicule = [];
 
+var iconVehicule = './drone.png';
+var iconFlote = '';
 
 function validate(){
-	
+  updateVehiculeLoc();
+  /*
 	var username = document.getElementById("username").value;
 	var pass = document.getElementById("password").value;
 	
@@ -15,7 +20,49 @@ function validate(){
 		}else{		
 			alert("Failled to Auth, try again.");
 		}		
-	});   
+	}); */
 	
 }
+
+
+function updateVehiculeLoc() {
+	
+  //Get data from server
+	  
+  //remove old marker
+  for (var i = 0; i < markersVehicule.length; i++) {
+    markersVehicule[i].setMap(null);
+  }
+  markersVehicule = [];
+  
+  //Set new
+  for (i = 0; i < 10; i++) {
+	
+	var myLatLng = {lat: (46.8516177)+(5-(Math.random()*10)), lng: (1.2393998)+(5-(Math.random()*10))};
+	 	
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		title: 'Hello World!',
+		icon: iconVehicule
+	});
+	
+	markersVehicule.push(marker);	
+	
+  }
+  
+}
+
+function initMap() {
+	
+	var franceLoc = {lat: 46.8516177, lng: 1.2393998};
+
+	map = new google.maps.Map(document.getElementById('map'), {
+	   zoom: 5,
+	   center: franceLoc,
+	   mapTypeId: google.maps.MapTypeId.TERRAIN
+	});
+   
+}
+
 
