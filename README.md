94ba4795af5f5b081890b73f98e3abc6877915db
Samy <zobiri_s@esme.fr>
2017-01-02 23:31:34 +0100

    Some update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Server autonomous vehicle
 
-Install instruction:
+##Install instruction:
 
 - go to the main floders
 - npm install
@@ -10,15 +10,28 @@ Possible call (CRUD and login):
 <br>(remplace localhost by the server ip)
 
 - Login: http://localhost:8080/login?user=YourUserName&pass=YourPassword This log you with the username and password pass in parameter
-- Create: http://localhost:8080/newVehicule This create a new vehicle (spawn near to the home point).
-- Read: http://localhost:8080/getVehiculeData This get a JSON with the data of all the vehicle.
-- Update: http://localhost:8080/changeVehiculeDest?id=YourId&lat=DestinationLat&lng=DestinationLng This set a new destination for the selected vehicle.
-- Delete: http://localhost:8080/deletVehicule?id=YourId This delet the selected vehicle. 
+- Create vehicle: http://localhost:8080/newVehicule This create a new vehicle (spawn near to the home point).
+- Create group: http://localhost:8080/newGroup This create a new group (Empty).
+- Read vehicle: http://localhost:8080/getVehiculeData This get a JSON with the data of all the vehicle.
+- Read group: http://localhost:8080/getGroupData This get a JSON with the data of all the group.
+- Update vehicle: http://localhost:8080/changeVehiculeDest?id=YourId&lat=DestinationLat&lng=DestinationLng This set a new destination for the selected vehicle.
+- Update group: http://localhost:8080/changeGroupDest?id=YourId&lat=DestinationLat&lng=DestinationLng This set a new destination for the selected group.
+- Update group: http://localhost:8080/addToGroup?gid=YourGroupId&vid=YourVheiculeID This add the vid vehicule to the gid group.
+- Update group: http://localhost:8080/rmToGroup?gid=YourGroupId&vid=YourVheiculeID This remove the vid vehicule to the gid group.
+- Delete vehicle: http://localhost:8080/deletVehicule?id=YourId This delet the selected vehicle. 
+- Delete group: http://localhost:8080/deletVehicule?id=YourId This delet the selected group. 
 
-Testing client :
+##Testing client:
 
 - Open index.html in the "Test client" floders (js have to be alowed by your webrowser)
 - Wait until google maps API is loaded
 - Login using the test creditential (Login "bob" Password "mdpbob")
 - Now you can use the left pannel to create new vehicle, send them were you want, groupe them, or delet them.
+
+##Rules:
+
+- There is a max number of vehicle (var maxVehicule can be change in server.js).
+- The last order erase the older (example if you set a destination to a vehicule and without waiting the end change for a new on, the vehicule go directly from where he is to the new one). 
+- You cant set new destination for a vehicule in a group (have to change the groupe destination, or remove him of the group for set his personal destination).
+- You cant have more group than vehicule (max number of groupe = actual number of vehicule)
 
--- serveur.js
+++ serveur.js
@@ -34,17 +34,34 @@ function travel_to(params){
 }
 
 
-//Function that return a unused ID
-var last_id = 0;
-function getFreeID(){	
-	last_id+=1;
-	return (last_id-1);
+//Function that return a unused V ID
+var last_v_id = 0;
+function getFreeVID(){	
+	last_v_id+=1;
+	return (last_v_id-1);
 }
 
 //Function that return a vehicule index coorespond to is ID 
 function getVehiculeById(id){
 	for (i = 0; i < vehiculeArray.length; i++) {	
 		if (vehiculeArray[i].id == id){
+			return i;
+		}
+	}
+	return -1;
+}
+
+//Function that return a unused G ID
+var last_g_id = 0;
+function getFreeGID(){	
+	last_g_id+=1;
+	return (last_g_id-1);
+}
+
+//Function that return a group index coorespond to is ID 
+function getGroupById(id){
+	for (i = 0; i < groupArray.length; i++) {	
+		if (groupArray[i].id == id){
 			return i;
 		}
 	}
@@ -96,8 +113,7 @@ setInterval(function(){
 			vehiculeArray[i].loc.lng = vehiculeArray[i].loc.lng - pas;
 		}else{
 			vehiculeArray[i].loc.lng = vehiculeArray[i].dest.lng;
-		}
-		
+		}		
 		
 	}  	
 }, 1000); 
@@ -125,6 +141,7 @@ var server = http.createServer(function(req, res) {
 	//Prepare the futur data to be send
 	var data;		
 	
+	//Il you are not loged
 	if (bob.isConnected){
 	
 		//CRUD Create Read Update Delete
@@ -138,7 +155,7 @@ var server = http.createServer(function(req, res) {
 			//Create Vehicule
 			case "/newVehicule":				
 				if (vehiculeArray.length < maxVehicule) {
-					var newId = getFreeID();
+					var newId = getFreeVID();
 					var rayonOfSpawn = 2.0;
 					var newlat = home.lat + rayonOfSpawn * Math.cos( (2 * Math.PI/180) * newId*12);
 					var newlng = home.lng + rayonOfSpawn * Math.sin( (2 * Math.PI/180) * newId*12);	
@@ -168,7 +185,13 @@ var server = http.createServer(function(req, res) {
 			
 			//Create Group
 			case "/newGroup":	
-			
+				if (groupArray.length < vehiculeArray.length) {
+					var newId = getFreeVID();
+					groupArray.push({id: newId, vehicule: []});
+					data = { succes : true };   					
+				} else {
+					data = { succes : false,  error : "You have to mutch group compare to your number of vehicule" };   
+				}
 			break;
 			
 			//Read Vehicule Data
@@ -178,7 +201,7 @@ var server = http.createServer(function(req, res) {
 			
 			//Read Group Data
 			case "/getGroupData":				
-				data = { succes : true, vehicule : vehiculeArray };   				
+				data = { succes : true, group : groupArray };   				
 			break;
 			
 			//Update Vehicule Dest
@@ -200,14 +223,59 @@ var server = http.createServer(function(req, res) {
 			
 			//Update Group Dest
 			case "/changeGroupDest":
+				if ('id' in params && 'lat' in params && 'lng' in params) {	
+					var index  = getGroupById(params['id']);
+					if (index > -1){
+						for (i = 0; i < groupArray[index].vehicule.length; i++) {	
+							var indexV  = getVehiculeById(groupArray[index].vehicule[i].id);
+							vehiculeArray[indexV].dest.lat = parseFloat(params['lat']);	
+							vehiculeArray[indexV].dest.lng = parseFloat(params['lng']);	
+						}
+					}else{
+						
+					}
+				}else{
+					data = { succes : false, error : "wrong params" };
+				}
+			break;			
 			
+			//Update Group Dest
+			case "/addToGroup":
+				if ('gid' in params && 'vid' in params) {	
+					var indexG = getGroupById(params['gid']);
+					var indexV = getGroupById(params['vid']);
+					if (indexG > -1 && indexV > -1){
+						groupArray[indexG].vehicule.push(vid);
+						data = { succes : true };
+					}else{
+						data = { succes : false,  error : "VID or GID not found" };
+					}					
+				}else{
+					data = { succes : false, error : "wrong params" };
+				}
+			break;
+			
+			//Update Group Dest
+			case "/rmToGroup":
+				if ('gid' in params && 'vid' in params) {	
+					var indexG = getGroupById(params['gid']);
+					var indexV = getGroupById(params['vid']);
+					if (indexG > -1 && indexV > -1){
+						groupArray[indexG].vehicule.splice(indexV , 1);
+						data = { succes : true };
+					}else{
+						data = { succes : false,  error : "VID or GID not found" };
+					}					
+				}else{
+					data = { succes : false, error : "wrong params" };
+				}
 			break;
 			
 			//Delete Vehicule
 			case "/deletVehicule":			
 				if ('id' in params) {							
 					var index  = getVehiculeById(params['id']);
-					console.log("delet: " + params['id'] + " at index: " + index);					
+					console.log("delet vehicule: " + params['id'] + " at index: " + index);					
 					if (index  > -1){
 						vehiculeArray.splice(index , 1);						
 						data = { succes : true };  
@@ -221,12 +289,25 @@ var server = http.createServer(function(req, res) {
 			
 			//Delete Group
 			case "/deletGroup":	
-			
+				if ('id' in params) {	
+					var index  = getGroupById(params['id']);
+					console.log("delet group: " + params['id'] + " at index: " + index);					
+					if (index  > -1){
+						groupArray.splice(index , 1);						
+						data = { succes : true };  
+					}else{
+						data = { succes : false,  error : "ID not found" };
+					}			
+				}else{
+					data = { succes : false, error : "wrong params" };
+				}
 			break;
 			
 		}
 	
 	}else{
+		
+	//Il you are not already loged
 		if (page == "/login"){
 			if(bob.auth(params,ip)){
 				data = { succes : true };  
@@ -237,6 +318,7 @@ var server = http.createServer(function(req, res) {
 			data = { succes : false, error : "You have to login for acces to this"};
 		}
 	}
+	
 	//Convert the data to JSON
 	var json = JSON.stringify(data); 
 	//Send the JSON to the client
